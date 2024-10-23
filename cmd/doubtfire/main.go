63a2d607715989b579bc42df95905bc9827435cf
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-10-23 16:34:14 +0300

    clear screen on start

--- cmd/doubtfire/main.go
+++ cmd/doubtfire/main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	fmt.Print("\033[2J") // clear screen
+	fmt.Print("\033[H")  // move cursor to top-left
 	if err := app.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
